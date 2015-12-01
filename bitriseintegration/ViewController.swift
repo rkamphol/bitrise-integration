e876c26b6c78bd668cd21071624337e5918e4fe5
Kamphol Rattanachanchai <kamphol@kitchenup.com>
2015-12-01 12:13:33 +0700

    test 4

--- bitriseintegration/ViewController.swift
+++ bitriseintegration/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("test 3")
+        print("test 4")
     }
 
     override func didReceiveMemoryWarning() {
