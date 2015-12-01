74e53c311c724ee39b353967b47c7895dca17ed4
Kamphol Rattanachanchai <kamphol@kitchenup.com>
2015-12-01 19:48:43 +0700

    enhance 6

--- bitriseintegration/ViewController.swift
+++ bitriseintegration/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("enhance 5")
+        print("enhance 6")
     }
 
     override func didReceiveMemoryWarning() {
