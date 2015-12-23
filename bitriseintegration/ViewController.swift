22667a917cc00f551e46baedc836249cab8edba2
Kamphol Rattanachanchai <kamphol@kitchenup.com>
2015-12-23 14:50:35 +0700

    test versioning 3

--- bitriseintegration/ViewController.swift
+++ bitriseintegration/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("test versioning 2")
+        print("test versioning 3")
     }
 
     override func didReceiveMemoryWarning() {
